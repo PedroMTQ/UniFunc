00a7578be656012c4fb68caeece1a7592f75e2dc
PedroMTQ <pdqueiros@gmail.com>
2021-12-27 11:43:42 +0100

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
+ from distutils.core import setup, Extension
+ import distutils.command.bdist_conda
 
+setup(
+    name="unifunc",
+    version="1.2",
+    distclass=distutils.command.bdist_conda.CondaDistribution,
+    conda_buildnum=0,
+)
